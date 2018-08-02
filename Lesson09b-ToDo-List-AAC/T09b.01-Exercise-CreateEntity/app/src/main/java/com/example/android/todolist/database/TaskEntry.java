254cc6844bc54fbc882fc1ce9ca7adc684cb4781
George Ampartzidis <ambageo@gmail.com>
2018-08-02 16:03:44 +0100

    Added Room annotations

--- Lesson09b-ToDo-List-AAC/T09b.01-Exercise-CreateEntity/app/src/main/java/com/example/android/todolist/database/TaskEntry.java
+++ Lesson09b-ToDo-List-AAC/T09b.01-Exercise-CreateEntity/app/src/main/java/com/example/android/todolist/database/TaskEntry.java
@@ -1,17 +1,30 @@
 package com.example.android.todolist.database;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
-// TODO (2) Annotate the class with Entity. Use "task" for the table name
+// completed (2) Annotate the class with Entity. Use "task" for the table name
+// Entity represents a single row of data in the table.
+// We can also name the table with the tableName attribute
+@Entity(tableName = "task")
 public class TaskEntry {
 
-    // TODO (3) Annotate the id as PrimaryKey. Set autoGenerate to true.
+    // completed (3) Annotate the id as PrimaryKey. Set autoGenerate to true.
+    // It is mandatory to declare a PrimaryKey. With autoGenerate set to true we let the database to
+    // automatically assign a different id to each row
+    @PrimaryKey(autoGenerate = true)
     private int id;
+
     private String description;
     private int priority;
     private Date updatedAt;
 
-    // TODO (4) Use the Ignore annotation so Room knows that it has to use the other constructor instead
+    // completed (4) Use the Ignore annotation so Room knows that it has to use the other constructor instead
+    // Room will use the second constructor (the one with the id) to create a TaskEntry object.
+    @Ignore
     public TaskEntry(String description, int priority, Date updatedAt) {
         this.description = description;
         this.priority = priority;
